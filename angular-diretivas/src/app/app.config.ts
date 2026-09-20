e1cb01356ae0275b2bffc21e1b43d274d4e07b1b
NixxsCarv <nicolas.carv29@hotmail.com>
2026-09-20 10:23:30 -0300

    Delete angular-diretivas directory

--- angular-diretivas/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
-  ]
-};
